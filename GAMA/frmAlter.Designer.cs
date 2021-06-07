d8e949b7dafeca39bde945efa862164b580cc982
zohre <zohrehShabanpour13@gmail.com>
2021-06-07 19:06:35 +0430

    control notification and toggle and raidioButton added

---
+++ GAMA/frmAlter.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace Test
+{
+    partial class frmAlter
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.picb = new System.Windows.Forms.PictureBox();
+            this.lblMessage = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.picb)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picb
+            // 
+            this.picb.Dock = System.Windows.Forms.DockStyle.Right;
+            this.picb.Location = new System.Drawing.Point(223, 0);
+            this.picb.Name = "picb";
+            this.picb.Size = new System.Drawing.Size(44, 52);
+            this.picb.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picb.TabIndex = 0;
+            this.picb.TabStop = false;
+            this.picb.Click += new System.EventHandler(this.Control_Click);
+            // 
+            // lblMessage
+            // 
+            this.lblMessage.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblMessage.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMessage.Location = new System.Drawing.Point(0, 0);
+            this.lblMessage.Name = "lblMessage";
+            this.lblMessage.Size = new System.Drawing.Size(223, 52);
+            this.lblMessage.TabIndex = 1;
+            this.lblMessage.Text = "label1";
+            this.lblMessage.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.lblMessage.Click += new System.EventHandler(this.Control_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 5000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // frmAlter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(267, 52);
+            this.Controls.Add(this.lblMessage);
+            this.Controls.Add(this.picb);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MinimumSize = new System.Drawing.Size(267, 52);
+            this.Name = "frmAlter";
+            this.Opacity = 0D;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Shown += new System.EventHandler(this.frmAlter_Shown);
+            this.Click += new System.EventHandler(this.Control_Click);
+            this.Resize += new System.EventHandler(this.frmAlter_Resize);
+            ((System.ComponentModel.ISupportInitialize)(this.picb)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picb;
+        private System.Windows.Forms.Label lblMessage;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
