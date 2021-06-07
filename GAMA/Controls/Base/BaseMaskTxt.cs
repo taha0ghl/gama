d19507a782e467ddd9134aff06b5fea6c7c5f020
zohre <zohrehShabanpour13@gmail.com>
2021-06-07 14:13:33 +0430

    frmAddEditSabtNam Modified

---
+++ GAMA/Controls/Base/BaseMaskTxt.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using MyClass;
+
+namespace GAMA
+{
+    class BaseMaskTxt : MaskedTextBox, IOrderable, IGAMAControl
+    {
+        public BaseMaskTxt()
+        {
+
+        }
+
+        //Properties***************************
+        #region 
+
+        public int ColumnIndex { get; set; }
+        public int RowIndex { get; set; }
+        public string PareName { get; set; }
+
+        #endregion
+        //*************************************
+
+        //Methods******************************
+        #region
+
+        public void AlignCenter(Control ctrl)
+        {
+            Locations.AlignCenters(ctrl, this);
+        }
+        public void AlignMiddle(Control ctrl)
+        {
+            Locations.AlignMiddles(ctrl, this);
+        }
+
+        #endregion
+        //*************************************
+    }
+}
